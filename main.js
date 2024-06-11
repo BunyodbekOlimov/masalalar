9487bc0a117ac69f2deb3c41e82b1ab582bd20af
BunyodbekOlimov <oybekivichbunyod@gmail.com>
2024-06-11 17:06:36 +0500

    start

---
+++ main.js
@@ -0,0 +1,111 @@
+// {
+//     // misol 1
+//     function peremetri (a) {
+//     return 4 * a;
+// } 
+// console.log(perimetri(5));
+// }
+
+
+// {
+//     // misol 2
+//     function yuzi(a) {
+//         return a * a;
+//     }
+    
+//     console.log(yuzi(5));
+// }
+
+
+// {
+//     // misol 3
+//     function yuzi(a, b) {
+//         return a * b;
+//     }
+    
+//     function perimetri(a, b) {
+//         return 2 * (a + b);
+//     }
+
+//     console.log(yuzi(5, 10));
+//     console.log(peremetri(5, 10));
+    
+// }
+
+
+// {
+//     // misol 4
+//     function uzunligi(d) {
+//         const pi = 3.14;
+//         return pi * d;
+//     }
+    
+//     console.log(uzunlig(10));    
+// }
+
+
+// {
+//     // misol 5
+//     function hajm(a) {
+//         return a ** 3;
+//     }
+    
+//     function sirti(a) {
+//         return 6 * (a ** 2);
+//     }
+    
+//     console.log(hajmi(3));
+//     console.log(sirti(3));    
+// }
+
+
+// {
+//     // misol 6
+//     function hajmi(a, b, c) {
+//         return a * b * c;
+//     }
+    
+//     function sirti(a, b, c) {
+//         return 2 * (a * b + b * c + a * c);
+//     }
+    
+//     console.log(hajmi(2, 3, 4)); 
+//     console.log(sirti(2, 3, 4));     
+// }
+
+
+// {
+//     // misol 7
+//     function uzunligi(R) {
+//         const pi = 3.14;
+//         return 2 * pi * R;
+//     }
+    
+//     function yuzasi(R) {
+//         const pi = 3.14;
+//         return pi * (R ** 2);
+//     }
+    
+//     console.log(uzunligi(5));
+//     console.log(yuzasi(5));    
+// }
+
+
+// {
+//     // misol 8
+//     function ortaArifmetigi(a, b) {
+//         return (a + b) / 2;
+//     }
+    
+//     console.log(ortaArifmetigi(10, 20));    
+// }
+
+
+// {
+//     // misol 9
+//     function geometri(a, b) {
+//         return Math.sqrt(a * b);
+//     }
+    
+//     console.log(geometri(9, 16));    
+// }
